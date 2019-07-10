0e9ce913845cc1e1ddf5b8451bbd67b25ef93cb4
kavisha <kavisha_jg@yahoo.com>
2019-07-10 11:25:06 +0530

    Complete Quizzler project

---
+++ Quizzler/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Kavisha on 09/07/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Question {
+    
+    let question : String;
+    let answer : Bool;
+    
+    init(text : String, correctAnswer : Bool) {
+        question = text;
+        answer = correctAnswer;
+    }
+    
+}
